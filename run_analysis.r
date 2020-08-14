ad1d2c0d892cab2ef6cf372e78cdf4ed774be26b
Matticus111 <mellis20@jhu.edu>
2020-08-14 10:36:36 -0400

    final commit

--- run_analysis.r
+++ run_analysis.r
@@ -1,2 +1,8 @@
-a <- "Hello World"
-print(a)
+# this is an r file to get and clean accelerometer data
+
+fileurl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl1, destfile = destfile = "/Users/matthewellis/Desktop/Coursera/A3/rprog_data_ProgAssignment3-data//Project4/datafile1.csv", method = "curl")
+dateDownloaded <- date()
+
+read.csv("/Users/matthewellis/Desktop/Coursera/A3/rprog_data_ProgAssignment3-data/datafile1.csv")
+
